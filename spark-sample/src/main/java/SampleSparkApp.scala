43e34d7ce7921b98167efc535bc7134dfd63e9ea
raghuchilukamari <raghunadh.chilukamari@gmail.com>
2019-10-23 00:23:54 -0700

    commit sample dataset operations

--- spark-sample/src/main/java/SampleSparkApp.scala
+++ spark-sample/src/main/java/SampleSparkApp.scala
@@ -1,18 +1,53 @@
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.{DoubleType, IntegerType, StringType}
+
+// For identifying schema
+case class Iris (incidentnum:String,
+category:String, description:String, dayofweek:String, date:String, time:String,
+pddistrict:String, resolution:String, address:String, x:Double, y:Double, pdid:String)
 
 object SampleSparkApp {
+
   def main(args: Array[String]): Unit = {
     val spark = SparkSession
       .builder()
       .appName("SampleSparkApp")
-      .config("spark.master","local")
-      .getOrCreate();
-    val df = spark.read.csv("/Users/rc/Documents/workspace/spark-sample/iris.csv");
-    df.show(5);
+      .master("local") // .config("spark.master","local")
+      .getOrCreate()
+
+    // For definitions of many spark functions
+    import spark.implicits._
+
+    // Load data into Dataframe
+    val columns=Array("incidentnum",
+      "category", "description", "dayofweek", "date", "time",
+      "pddistrict", "resolution", "address", "x", "y", "pdid")
+    val sfpdDf = spark.read.format("csv")
+      .option("inferschema","true")
+      .load("/Users/rc/workspaces/spark/Data/sfpd.csv")
+      .toDF(columns: _*)
+
+    //Convert Dataframe to Dataset
+    val sfpdDS = sfpdDf.as[Iris]
+    sfpdDS.show(5)
+
+    //Register Dataset as table -  enables   to query it using SQL.
+    sfpdDS.createTempView("sfpd")
+
+    spark.sql("select * from sfpd limit 5").show()
+
+    // Exploring Dataset:
+
+    // top 5 districts with most incidents
+    val incByDistDS = sfpdDS.groupBy("pddistrict") // groupby gives a relational grouped dataset, count is more a transformation than action here
+      .count()
+      .sort($"count".desc)
+
+    incByDistDS.write.format("json")
+      .mode("overwrite")
+      .save("/Users/rc/workspaces/spark/Data/output/")
+
+
 
   }
-
-
-
 }
